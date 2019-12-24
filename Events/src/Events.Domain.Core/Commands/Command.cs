0c3cea2bf29feae38c0d00557788b8fe7dc1d5fc
Marcelo Nespoli Santos <marcelonespoli@outlook.com>
2019-12-24 19:38:16 +0000

    Implemented domain core classes

---
+++ Events/src/Events.Domain.Core/Commands/Command.cs
@@ -0,0 +1,16 @@
+﻿using Events.Domain.Core.Events;
+using MediatR;
+using System;
+
+namespace Events.Domain.Core.Commands
+{
+    public class Command : Message, IRequest 
+    {
+        public DateTime Timestamp { get; private set; }
+
+        public Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
---
+++ Events/src/Events.Domain.Core/Events/StoredEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Events.Domain.Core.Events
+{
+    public class StoredEvent : Event
+    {
+        public Guid Id { get; private set; }
+        public string Data { get; private set; }
+        public string User { get; private set; }
+        
+        // EF Constructor
+        protected StoredEvent() { }
+
+        public StoredEvent(Event runEvent, string data, string user)
+        {
+            Id = Guid.NewGuid();
+            AggregateId = runEvent.AggregateId;
+            MessageType = runEvent.MessageType;
+            Data = data;
+            User = user;
+        }
+        
+    }
+}
