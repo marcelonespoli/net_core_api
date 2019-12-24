214429132144f96635ac9ed5cfeda5741dde6781
Marcelo Nespoli Santos <marcelonespoli@outlook.com>
2019-12-24 21:08:47 +0000

    Added handlers

---
+++ Conferences/src/Conferences.Domain/Interfaces/IEventStore.cs
@@ -0,0 +1,9 @@
+﻿using Conferences.Domain.Core.Events;
+
+namespace Conferences.Domain.Interfaces
+{
+    public interface IEventStore
+    {
+        void SaveEvent<T>(T @event) where T : Event;
+    }
+}
