823aa28cb3197299bdcb36ce8e9a229229288568
Marcelo Nespoli Santos <marcelonespoli@outlook.com>
2019-12-24 22:18:20 +0000

    Added Interface for repositories Added Organizer handlers

---
+++ Conferences/src/Conferences.Domain/Organizers/Repository/IOrganizerRepository.cs
@@ -0,0 +1,12 @@
+﻿using Conferences.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conferences.Domain.Organizers.Repository
+{
+    public interface IOrganizerRepository: IRepository<Organizer>
+    {
+
+    }
+}
