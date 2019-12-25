bf4e1a8366c324704e994362105ba65d9569a863
Marcelo Nespoli Santos <marcelonespoli@outlook.com>
2019-12-25 17:02:53 +0000

    Added Entitiy Framework Added DB contexts Added Mappings

---
+++ Conferences/src/Conferences.Infra.Data/Mappings/CategoryMapping.cs
@@ -0,0 +1,21 @@
+﻿using Conferences.Domain.Conferences;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Conferences.Infra.Data.Mappings
+{
+    public class CategoryMapping : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.Property(p => p.Name)
+                .HasColumnType("varchar(150)")
+                .IsRequired();
+
+            builder.Ignore(p => p.ValidationResult);
+            builder.Ignore(p => p.CascadeMode);
+
+            builder.ToTable("Categories");
+        }
+    }
+}
---
+++ Conferences/src/Conferences.Infra.Data/Mappings/StoredEventMapping.cs
@@ -0,0 +1,22 @@
+﻿using Conferences.Domain.Core.Events;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Conferences.Infra.Data.Mappings
+{
+    public class StoredEventMapping : IEntityTypeConfiguration<StoredEvent>
+    {
+        public void Configure(EntityTypeBuilder<StoredEvent> builder)
+        {
+            builder.Property(p => p.Timestamp)
+                .HasColumnName("CreationDate");
+
+            builder.Property(p => p.MessageType)
+                .HasColumnName("Action")
+                .HasColumnType("varchar(100)");
+
+            builder.Property(c => c.User)
+                .HasColumnType("varchar(300)");
+        }
+    }
+}
