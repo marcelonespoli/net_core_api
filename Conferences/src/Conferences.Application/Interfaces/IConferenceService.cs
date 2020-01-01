b04c1b4066e27380365d21fa742d75b1989f4023
Marcelo Nespoli Santos <marcelonespoli@outlook.com>
2020-01-01 16:03:56 +0000

    Added app services and fixed packages version

---
+++ Conferences/src/Conferences.Application/Interfaces/IConferenceService.cs
@@ -0,0 +1,20 @@
+﻿using Conferences.Application.ViewModels;
+using Conferences.Domain.Conferences.Commands;
+using System;
+using System.Collections.Generic;
+
+namespace Conferences.Application.Interfaces
+{
+    public interface IConferenceService
+    {
+        IEnumerable<ConferenceViewModel> GetAll();        
+        IEnumerable<CategoryViewModel> GetCategories();
+        IEnumerable<ConferenceViewModel> GetMyConferences(Guid id, Guid organizerId);
+        ConferenceViewModel GetById(Guid id);
+        RegisterConferenceCommand RegisterConference(ConferenceViewModel conferenceViewModel);
+        UpdateConferenceCommand UpdateConference(ConferenceViewModel conferenceViewModel);
+        ExcludeConferenceCommand DeleteConference(Guid id);
+        AddAddressConferenceCommand AddAddress(AddressViewModel addressViewModel);
+        UpdateAddressConferenceCommand UpdateAddress(AddressViewModel addressViewModel);
+    }
+}
